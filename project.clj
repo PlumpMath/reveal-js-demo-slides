fca488e82fd8c6f04a5eebea6a2b5762e9ad7032
nick rothwell <nick@cassiel.com>
2013-05-26 18:33:13 +0100

    Example slides and media.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject reveal-js-demo-slides "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Demo Slides for reveal.js"
+  :url "https://github.com/cassiel/reveal-js-demo-slides"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[hiccup "1.0.3"]
+                 [org.clojure/clojure "1.5.1"]])
